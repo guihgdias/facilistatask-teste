8ecc4e25db7a5f0bd488292200fadc5dae6b14b1
Guilherme Henrique <guilhermehgdiasbr@gmail.com>
2021-06-02 04:15:09 -0300

    Added action to add task button

--- scripts/components/addtask.js
+++ scripts/components/addtask.js
@@ -5,8 +5,12 @@ const buttonAddTask = document.querySelector('.addNewTask');
 
 buttonOpenModal.addEventListener('click', () => {
     modalAddTask.classList.remove('-hidden');
-})
+});
 
 buttonCloseModal.addEventListener('click', () => {
+    modalAddTask.classList.add('-hidden');
+});
+
+buttonAddTask.addEventListener('click', () => {
     modalAddTask.classList.add('-hidden');
 })
