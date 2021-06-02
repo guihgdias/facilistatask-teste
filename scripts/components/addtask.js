24248aa6de26f896dbe68043deb459034edca9d5
Guilherme Henrique <guilhermehgdiasbr@gmail.com>
2021-06-02 03:59:52 -0300

    Send project to github

---
+++ scripts/components/addtask.js
@@ -0,0 +1,12 @@
+const buttonOpenModal = document.querySelector('.addtask-button');
+const buttonCloseModal = document.querySelector('.closeModal');
+const modalAddTask = document.querySelector('#addTaskModal');
+const buttonAddTask = document.querySelector('.addNewTask');
+
+buttonOpenModal.addEventListener('click', () => {
+    modalAddTask.classList.remove('-hidden');
+})
+
+buttonCloseModal.addEventListener('click', () => {
+    modalAddTask.classList.add('-hidden');
+})
