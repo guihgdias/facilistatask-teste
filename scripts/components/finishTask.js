8ebfcc1e0a853acdb3cd66d29eeedb9533b85da0
Guilherme Henrique <guilhermehgdiasbr@gmail.com>
2021-06-04 00:40:55 -0300

    End task function completed

---
+++ scripts/components/finishTask.js
@@ -0,0 +1,15 @@
+function finishTask(idTask) {
+    let thisTask = document.getElementById(''+idTask+'')
+    let thisCheck = thisTask.querySelector('.checkButton')
+    let thisName = thisTask.querySelector('#taskName')
+
+    if(thisCheck.classList.contains('-active') == false) {
+        thisCheck.classList.add('-active')
+        thisTask.style = 'opacity: 50%'
+        thisName.style = 'text-decoration: line-through'
+    } else {
+        thisCheck.classList.remove('-active')
+        thisTask.style = 'opacity: 100%'
+        thisName.style = 'text-decoration: none'
+    }
+}
