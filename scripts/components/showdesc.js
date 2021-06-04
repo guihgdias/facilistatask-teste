1c8c3d6d1cf1586f57123c0ce4ced16e60f4a699
Guilherme Henrique <guilhermehgdiasbr@gmail.com>
2021-06-03 23:12:11 -0300

    Removing files with invalid names, new files with default names have been put in their place

--- scripts/components/showdesc.js
+++
@@ -1,9 +0,0 @@
-function show(idTask) {
-    let thisTask = document.getElementById(''+idTask+'');
-
-    if(thisTask.classList.contains('-open')) {
-        thisTask.classList.remove('-open')
-    } else {
-        thisTask.classList.add('-open')
-    }
-}
